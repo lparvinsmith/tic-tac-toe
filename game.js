83b9c08f4ef11e9459840e49c0eb2eb02c68e4bc
Lara Parvinsmith <lara.parvinsmith@gmail.com>
2015-07-01 15:36:31 -0400

    adds file game.js with minimal code

---
+++ game.js
@@ -0,0 +1,19 @@
+'use strict;'
+
+//start as object instead, with square# as keys and value of null/X/O?
+var board = [ square1, square2, square3,
+              square4, square5, square6,
+              square7, square8, square9];
+
+var selectFirstPlayer = function() {
+  var firstPlayer;
+  if () {
+    firstPlayer = playerX;
+  }
+  else {
+    firstPlayer = playerO;
+  }
+  return firstPlayer;
+}
+
+var
