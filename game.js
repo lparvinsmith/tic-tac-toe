9d305a4b76127a6331926592e88308bacd07ee62
Lara Parvinsmith <lara.parvinsmith@gmail.com>
2015-07-03 12:53:38 -0400

    fixes some functions in game.js, tests look good. still need to fix function threeInARow to be able to tell WHICH player has won. also fixes board divs in CSS.

--- game.js
+++ game.js
@@ -1,26 +1,35 @@
 'use strict;'
 
 
-var board = { square1: null, square2: null, square3: null,
-              square4: null, square5: null, square6: null,
-              square7: null, square8: null, square9: null};
+var board = { square1: 'X', square2: 'O', square3: "O",
+              square4: 'O', square5: 'X', square6: 'X',
+              square7: 'X', square8: "O", square9: 'X'};
 
-//alternate which player begins, starting with playerX
+//FIX THIS alternate which player begins, starting with playerX
 var selectFirstPlayer = function() {
+  var playerX;
+  var playerO;
   var firstPlayer;
-  if (firstPlayer !==playerX) {
+  if (firstPlayer !== playerX || firstPlayer === undefined) {
     firstPlayer = playerX;
   }
   else {
     firstPlayer = playerO;
   }
+  console.log(firstPlayer + ' begins');
   return firstPlayer;
 }
 
+// selectFirstPlayer();
+
 //when player clicks square, corresponding key in object board is given value X or O
+//if div1 is clicked by playerX, square1 takes value 'X'
+//if div1 is clicked by playerO, square1 takes value 'O'
+//once div1 is clicked and its value set, it cannot change value until clearBoard()
 
 //determines when a game is over and if there is a winner or a tie
 //FIX for when values are not defined and for how to figure out if X or O
+//TRY with function(player) { if square1 === player && square2 === player && square3 === player}
 var threeInARow = function() {
   var winner;
   if (board.square1 === board.square2 && board.square1 === board.square3 ||
@@ -35,19 +44,23 @@ var threeInARow = function() {
     console.log('there is a winner!');
     return winner;
   }
-  for (value in board) { //how do I check if values are all truthy, or none are null?
-    if (board.value) { //need to apply to all values in object board
-    winner = "draw";
-    return winner;
+  //if all values in board are truthy, winner = 'draw';
+  for (key in board) {
+    var value = board[key];
+    if (value !== null) {
+      winner = "draw";
+      return winner;
     }
   }
 }
 
+console.log(threeInARow());
+
 //stores outcome in scoreboard
 var keepScore = function() {
-  var playerXScore;
-  var playerOScore;
-  var draw;
+  var playerXScore = 0;
+  var playerOScore = 0;
+  var draw = 0;
   if (winner === playerX) {
     playerXScore++;
   }
@@ -59,9 +72,13 @@ var keepScore = function() {
   }
 }
 
-//if button play again is pressed, clears board
+//if button 'play again' is pressed, clears board
 var clearBoard = function() {
-  for (value in board) { //like in threeInARow function, how do i touch all values only?
-    value = null;
+  for (key in board) { //should touch all values
+    board[key] = null;
   }
 }
+
+//woo it works!
+// clearBoard();
+// console.log(board);
