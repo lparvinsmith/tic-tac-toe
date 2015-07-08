ec6ebbe3ee56d180a85148b91deec018bbe95c38
Lara Parvinsmith <lara.parvinsmith@gmail.com>
2015-07-08 14:54:45 -0400

    changes board from object to array, and updates all related code in game.js, as well as fixes threeInARow to be able to tell if there's a draw. also updates index.html and style.css to rename divs after boxes0-8, to correspond better to array indeces.

--- game.js
+++ game.js
@@ -1,65 +1,69 @@
 'use strict';
 
-var board = { square1: 'X', square2: null, square3: null,
-              square4: null, square5: "X", square6: null,
-              square7: null, square8: null, square9: "X"};
+var board = ["x", "o", "x",
+             "x", "o", "o",
+             "o", "o", "x",];
 
 var threeInARow = function(player) {
   var winner;
   //if three squares in a row are equal to player, that player wins
-  if (board.square1 === player &&
-      board.square2 === player &&
-      board.square3 === player) {
+  if (board[0] === player &&
+      board[1] === player &&
+      board[2] === player) {
     winner = player;
   }
-  if (board.square1 === player &&
-      board.square5 === player &&
-      board.square9 === player) {
+  else if (board[0] === player &&
+           board[4] === player &&
+           board[8] === player) {
     winner = player;
   }
-  if (board.square1 === player &&
-      board.square4 === player &&
-      board.square7 === player) {
+  else if (board[0] === player &&
+           board[3] === player &&
+           board[6] === player) {
     winner = player;
   }
-   if (board.square2 === player &&
-       board.square5 === player &&
-       board.square8 === player) {
+  else if (board[1] === player &&
+           board[4] === player &&
+           board[7] === player) {
     winner = player;
   }
-   if (board.square3 === player &&
-       board.square5 === player &&
-       board.square7 === player) {
+  else if (board[2] === player &&
+           board[4] === player &&
+           board[6] === player) {
     winner = player;
   }
-   if (board.square3 === player &&
-       board.square6 === player &&
-       board.square9 === player) {
+  else if (board[2] === player &&
+           board[5] === player &&
+           board[8] === player) {
     winner = player;
   }
-   if (board.square4 === player &&
-       board.square5 === player &&
-       board.square6 === player) {
+  else if (board[3] === player &&
+           board[4] === player &&
+           board[5] === player) {
     winner = player;
   }
-   if (board.square7 === player &&
-       board.square8 === player &&
-       board.square9 === player) {
+  else if (board[6] === player &&
+           board[7] === player &&
+           board[8] === player) {
     winner = player;
   }
 //if all values in board are truthy, winner = 'draw';
-//this was working before i thought?? FIX ME
-  for (var key in board) {
-    var value = board[key];
-    if (value !== null) {
-      winner = "draw";
-    }
+  else if (board[0] &&
+           board[1] &&
+           board[2] &&
+           board[3] &&
+           board[4] &&
+           board[5] &&
+           board[6] &&
+           board[7] &&
+           board[8]) {
+    winner = 'draw';
   }
   console.log('the winner is player ' + winner);
   return winner;
 }
 
-threeInARow('X');
+threeInARow('o');
 
 var takeTurn = function(player) {
   //use ui.js to listen for click from player --HOW
@@ -104,12 +108,11 @@ var keepScore = function() {
 
 //if button 'play again' is pressed, clears board
 var clearBoard = function() {
-  for (key in board) { //should touch all values
-    board[key] = null;
+  for (var i = 0; i < board.length; i++) { //should touch all values
+    board[i] = "";
   }
 }
 
-//woo it works!
 // clearBoard();
 // console.log(board);
 
