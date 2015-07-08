8f9090bcabd981482a1faf4cebe5739b15d41818
Lara Parvinsmith <lara.parvinsmith@gmail.com>
2015-07-07 23:15:44 -0400

    changes threeInARow function, which now determines who won via brute force, but needs help for determining when there is a draw.

--- game.js
+++ game.js
@@ -1,14 +1,78 @@
-'use strict;'
+'use strict';
 
+var board = { square1: 'X', square2: null, square3: null,
+              square4: null, square5: "X", square6: null,
+              square7: null, square8: null, square9: "X"};
 
-var board = { square1: null, square2: null, square3: null,
-              square4: null, square5: null, square6: null,
-              square7: null, square8: null, square9: null};
+var threeInARow = function(player) {
+  var winner;
+  //if three squares in a row are equal to player, that player wins
+  if (board.square1 === player &&
+      board.square2 === player &&
+      board.square3 === player) {
+    winner = player;
+  }
+  if (board.square1 === player &&
+      board.square5 === player &&
+      board.square9 === player) {
+    winner = player;
+  }
+  if (board.square1 === player &&
+      board.square4 === player &&
+      board.square7 === player) {
+    winner = player;
+  }
+   if (board.square2 === player &&
+       board.square5 === player &&
+       board.square8 === player) {
+    winner = player;
+  }
+   if (board.square3 === player &&
+       board.square5 === player &&
+       board.square7 === player) {
+    winner = player;
+  }
+   if (board.square3 === player &&
+       board.square6 === player &&
+       board.square9 === player) {
+    winner = player;
+  }
+   if (board.square4 === player &&
+       board.square5 === player &&
+       board.square6 === player) {
+    winner = player;
+  }
+   if (board.square7 === player &&
+       board.square8 === player &&
+       board.square9 === player) {
+    winner = player;
+  }
+//if all values in board are truthy, winner = 'draw';
+//this was working before i thought?? FIX ME
+  for (var key in board) {
+    var value = board[key];
+    if (value !== null) {
+      winner = "draw";
+    }
+  }
+  console.log('the winner is player ' + winner);
+  return winner;
+}
+
+threeInARow('X');
+
+var takeTurn = function(player) {
+  //use ui.js to listen for click from player --HOW
+  //check if there are threeInARow
+}
+
+//need separate function for determining player order/switching?
+var switchTurns = function(player) {
+
+}
 
 //FIX THIS alternate which player begins, starting with playerX
 var selectFirstPlayer = function() {
-  var playerX;
-  var playerO;
   var firstPlayer;
   if (firstPlayer !== playerX || firstPlayer === undefined) {
     firstPlayer = playerX;
@@ -22,49 +86,15 @@ var selectFirstPlayer = function() {
 
 // selectFirstPlayer();
 
-//when player clicks square, corresponding key in object board is given value X or O
-//if div1 is clicked by playerX, square1 takes value 'X'
-//if div1 is clicked by playerO, square1 takes value 'O'
-//once div1 is clicked and its value set, it cannot change value until clearBoard()
-
-//determines when a game is over and if there is a winner or a tie
-//FIX for when values are not defined and for how to figure out if X or O
-//TRY with function(player) { if square1 === player && square2 === player && square3 === player}
-var threeInARow = function() {
-  var winner;
-  if (board.square1 === board.square2 && board.square1 === board.square3 ||
-      board.square1 === board.square5 && board.square1 === board.square9 ||
-      board.square1 === board.square4 && board.square1 === board.square7 ||
-      board.square2 === board.square5 && board.square2 === board.square8 ||
-      board.square3 === board.square5 && board.square3 === board.square7 ||
-      board.square3 === board.square6 && board.square3 === board.square9 ||
-      board.square4 === board.square5 && board.square4 === board.square6 ||
-      board.square7 === board.square8 && board.square7 === board.square9) {
-    //if common value is X, winner is playerX. if value is O, winner is playerO
-    console.log('there is a winner!');
-    return winner;
-  }
-  //if all values in board are truthy, winner = 'draw';
-  for (key in board) {
-    var value = board[key];
-    if (value !== null) {
-      winner = "draw";
-      return winner;
-    }
-  }
-}
-
-console.log(threeInARow());
-
 //stores outcome in scoreboard
 var keepScore = function() {
   var playerXScore = 0;
   var playerOScore = 0;
   var draw = 0;
-  if (winner === playerX) {
+  if (winner === 'X') {
     playerXScore++;
   }
-  else if (winner === playerO) {
+  else if (winner === 'O') {
     playerOScore++;
   }
   else {
